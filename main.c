8044c9dc044b06b2b1c14a33e84a63f63d53a54d
Victor Luna <vht.luna@gmail.com>
2022-09-13 09:23:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,10 @@
 #include "../bibliotecas/config.h"
-#include "../bibliotecas/lcd8bits.h"
 #include <xc.h>
 #define _XTAL_FREQ 8000000
 
 void main(void) {
-    // Desabilita I/O analógicas
+    // Desabilita I/O analÃ³gicas
     ADCON1 = 0x0F;
-    
-    // Lcd Modo 8 bits
-    iniciaLcd();
-    Lcd_Out(1, 1, "COMPARE 1KHZ");
     
     // Configura TMR1 Prescaler 1:2
     T1CON = 0b11010000;
@@ -17,7 +12,7 @@ void main(void) {
     TMR1L = 0;
     T1CONbits.TMR1ON = 1;
             
-    // Configuração do CCP
+    // ConfiguraÃ§Ã£o do CCP
     CCP1CON = 0b00000010;
     CCPR1H = 0x01;
     CCPR1L = 0xF4;
